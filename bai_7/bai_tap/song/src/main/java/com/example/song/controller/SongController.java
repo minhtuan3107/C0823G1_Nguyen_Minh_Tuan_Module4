68f891524618727022475903bcddf5c138907816
minhtuan3107 <minhtuan3107+tn989993@users.noreply.github.com>
2023-12-28 08:43:29 +0700

    nop bai tap ss7

--- bai_7/bai_tap/song/src/main/java/com/example/song/controller/SongController.java
+++ bai_7/bai_tap/song/src/main/java/com/example/song/controller/SongController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -45,21 +46,23 @@ public class SongController {
     }
 
     @GetMapping("edit/{id}")
-    private String formAdd(Model model, @PathVariable int id) {
-        model.addAttribute("songDTO", songService.findById(id));
+    private String formEdit(Model model, @PathVariable int id) {
+        Song song = songService.findById(id);
+        SongDTO songDTO = new SongDTO();
+        BeanUtils.copyProperties(song, songDTO);
+        model.addAttribute("songDTO", songDTO);
         return "edit";
     }
 
-    @GetMapping("editSong")
-    public String edit(@Valid @ModelAttribute SongDTO songDTO, BindingResult bindingResult, RedirectAttributes attributes) {
-        Song song = new Song();
-        new SongDTO().validate(songDTO, bindingResult);
-        if (bindingResult.hasFieldErrors()) {
+    @PostMapping("editSong")
+    public String edit(@Valid @ModelAttribute SongDTO songDTO, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
             return "edit";
         }
+        Song song = new Song();
         BeanUtils.copyProperties(songDTO, song);
-        songService.save(song);
-        attributes.addFlashAttribute("mess", "edit Success");
+        System.out.println(song.toString());
+        this.songService.save(song);
         return "redirect:/";
     }
 }
